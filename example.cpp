f7acdf22d91aa561137422a50bf2be45ca1c3149
Peter <graphhopper@gmx.de>
2019-11-21 22:32:11 +0100

    make coordinates dynamic

--- example.cpp
+++ example.cpp
@@ -12,10 +12,16 @@ int main(int argc, char **argv) {
         return 1;
   }
   // End boilerplate
+  if(argc != 4 + 1) {
+     std::cerr << "please specify coordinates\n./example latStart lonStart latEnd lonEnd" << std::endl;
+     return 1;
+  }
 
   double lat1 = stod(argv[1]);
-  double lon1 = stod(argv[2]); // e.g. 52.5147, 13.3883
-  double distance = runGH(thread, 52.5169, 13.3884, lat1, lon1);
+  double lon1 = stod(argv[2]);
+  double lat2 = stod(argv[3]);
+  double lon2 = stod(argv[4]);
+  double distance = runGH(thread, lat1, lon1, lat2, lon2);
 
   std::cout << "Distance " << distance << std::endl;
 
